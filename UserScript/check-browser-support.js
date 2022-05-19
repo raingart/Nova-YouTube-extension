7d14ff4eaa2d76187a46c29f366466ac1a81bd07
null <raingart@users.noreply.github.com>
2022-05-19 18:42:15 +0300

    current

--- UserScript/check-browser-support.js
+++
@@ -1,7 +0,0 @@
-try {
-   // throw 'test';
-   document?.body;
-} catch (error) {
-   return alert(GM_info.script.name + ' Error!\nYour browser does not support chaining operator.');
-}
-if (GM_info?.scriptHandler == 'Greasemonkey') alert(GM_info.script.name + ' Error!\nGreasemonkey is not supported.');
